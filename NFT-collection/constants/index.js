ad16a84de39c85fc162c8a6e20d2989e801c0fe4
W0rld3x <franckimandu81@gmail.com>
2022-05-27 13:58:36 +0200

    Add Folder

---
+++ NFT-collection/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xB740334cc5F964Eb8a79bc0E0Fe679c2b146FA09";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
