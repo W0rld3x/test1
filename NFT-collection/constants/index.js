a242801d16fd99dcee57940352eeebb04302c317
W0rld3x <franckimandu81@gmail.com>
2022-05-27 14:52:21 +0200

    added

---
+++ NFT-collection/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xeD018Fc58FB1b3e84a5b270e857c91e6647b6548";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nftcollection-gamma.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
